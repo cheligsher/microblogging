f665ec232b1d11a68471aca496ba18abc45f5716
cheligsher <cheligsher@gmail.com>
2022-12-04 17:16:59 +0200

    updated progress

--- src/components/AddTweet.jsx
+++ src/components/AddTweet.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button'
 
 function AddTweet() {
+    const [text, setText] = useState("")
+    
   return (
     <form>
       <div className="add-tweet-div">
-        <textarea placeholder="What do you have in mind..." rows={8}></textarea>
+        <textarea 
+        placeholder="What do you have in mind..." 
+        rows={8}
+        value= {text}
+        onChange={(e)=> setText(e.target.value)}
+        ></textarea>
         <button>Tweet</button>
       </div>
     </form>
